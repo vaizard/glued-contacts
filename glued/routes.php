6a626fa886a1d4838ba898d67c98eafa48988ab0
Pavel Stratil <pavel@vaizard.org>
2023-01-22 16:46:58 +0000

    fix rountes

--- glued/routes.php
+++ glued/routes.php
@@ -1,8 +1,18 @@
 <?php
 
+/** @noinspection PhpUndefinedVariableInspection */
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Exception\HttpNotFoundException;
+
+
+/**
+ * Catch-all route to serve a 204 options response
+ */
+$app->options('/{routes:.+}', function (Request $request, Response $response, $args) {
+    return $response->withStatus(204);
+})->setName('fallback_options');
+
 
 foreach ($settings['routes'] as $name => $leaf) {
     if (isset($leaf['methods'])) {
@@ -13,13 +23,6 @@ foreach ($settings['routes'] as $name => $leaf) {
     }
 }
 
-/**
- * Catch-all route to serve a a 204 options response
- * NOTE: make sure this route is defined last
- */
-$app->options('/{routes:.+}', function (Request $request, Response $response, $args) {
-    return $response->withCode(204);
-})->setName('fallback_options');
 
 /**
  * Catch-all route to serve a 404 Not Found page if none of the routes match
@@ -28,5 +31,3 @@ $app->options('/{routes:.+}', function (Request $request, Response $response, $a
 $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function (Request $request, Response $response) {
     throw new HttpNotFoundException($request);
 })->setName('fallback_notfound');
-
-// $app->get('/api/skeleton/devtest', Glued\Controllers\ServiceController::class . ':health')->setName('devtest');
